a7f6868c4a30fd469a1f0e895b30277415b241a9
Andrea Konomi <andreakonomi23@gmail.com>
2022-03-21 01:37:30 +0100

    further refactored entry logic to new class engine

--- SearchEngine.Console/Program.cs
+++ SearchEngine.Console/Program.cs
@@ -1,96 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
-using SearchEngine.Library.DataAccess;
-using SearchEngine.Library.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace SearchEngine.Cmd
+﻿namespace SearchEngine.Cmd
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string input;
-            input = PromptUser();
-
-            while (input.ToLower() != "Exit".ToLower())
-            {
-                Console.WriteLine(ProccessInput(input));
-                input = PromptUser();
-            }
-
-        }
-
-        private static string PromptUser()
-        {
-            Console.Write("Press 'Exit' or insert a token: ");
-            return Console.ReadLine();
-        }
-
-        private static string ProccessInput(string input)
-        {
-            try
-            {
-                if (string.IsNullOrWhiteSpace(input))
-                {
-                    return "You didn't input anything.";
-                }
-
-                // move this
-                string connString = Helper.GetConnectionString("Demo_Db");
-
-                var document = ParseDocument(input);
-                var docData = new DocumentData(connString);
-
-                docData.CreateDocument(document);
-
-                return $"index ok {document.Id}";
-            }
-            catch (Exception ex)
-            {
-                return $"index error {ex.Message}";
-            }
-        }
-
-        static DocumentForCreationModel ParseDocument(string input)
-        {
-            var docCreation = new DocumentForCreationModel();
-            var tokensArray = input.Split(' ');
-
-            bool ok = int.TryParse(tokensArray[0], out int id);
-
-            docCreation.Id = id;
-            docCreation.Tokens = ParseContent(tokensArray);
-
-            return docCreation;
-        }
-
-        static ICollection<TokenForCreationModel> ParseContent(string[] token)
-        {
-            string tokenContent = "";
-            bool valid = true;
-
-            List<TokenForCreationModel> tokens = new();
-            for (int i = 1; i < token.Length; i++)
-            {
-                tokenContent = token[i];
-                valid = CheckTokenIfValid(tokenContent);
-
-                if (!valid)
-                {
-                    throw new FormatException("All tokens need to be alphanumerical.");
-                }
-
-                tokens.Add(new TokenForCreationModel { Content = token[i] });
-            }
-
-            return tokens;
-        }
-
-        static bool CheckTokenIfValid(string token)
-        {
-            return token.All(x => char.IsLetterOrDigit(x));
+            Engine.Run();
         }
     }
 }
